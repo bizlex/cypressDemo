4d9479a5526f93618e462e6dc20f170f8195ca39
Alexey Levchuk <bizlexx@gmail.com>
2024-05-27 19:44:48 +0200

    feat: add mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -4,11 +4,13 @@ module.exports = defineConfig({
   e2e: {
     // Configure your E2E tests here
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,ts}',
-    reporter: 'mocha-junit-reporter',
+    reporter: 'mochawesome',
     reporterOptions: {
-      mochaFile: './results/junit_results.xml',
-      // jenkinsMode: true,
-      toConsole: true,
+      reportDir: "./results/mochawesome_results",
+      overwrite: false,
+      html: false,
+      json: true,
+      timestamp: "mmddyyyy_HHMMss"
     }
   },
 })
