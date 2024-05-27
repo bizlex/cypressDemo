f2d69ce7304521d37fb84edae5c433661c7670a7
Alexey Levchuk <bizlexx@gmail.com>
2024-05-27 17:53:36 +0200

    feat: add mocha-junit-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,12 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     // Configure your E2E tests here
-    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,ts}"
+    specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,ts}',
+    reporter: 'mocha-junit-reporter',
+    reporterOptions: {
+      mochaFile: './results/junit_results.xml',
+      // jenkinsMode: true,
+      toConsole: true,
+    }
   },
 })
